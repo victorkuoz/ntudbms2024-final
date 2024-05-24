52c1c4a126cb33c54f60cb5a905cbc8dd56d7cb6
Michael <mmm323790@gmail.com>
2024-05-24 15:02:07 +0800

    update backend for CORS

--- backend/index.py
+++ backend/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from utility import *
 
 import io
@@ -6,12 +7,27 @@ import soundfile as sf
 
 app = FastAPI()
 
+# Define the origins that should be allowed to make requests to this API
+origins = [
+    "http://localhost:3000",  # Add your frontend URL here
+    "http://127.0.0.1:3000",  # If you're using a different port, include it as well
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.post("/audio_query")
 async def audio_query(audio: UploadFile):
     content, sampling_rate = sf.read(io.BytesIO(await audio.read()))
     embedding = audio_embedding([content])[0]
     result = audio_embedding_search(embedding)
-
+    # print("POST: /audio_query")
     return {'result': result}
 
 @app.post("/audio_embedding_query")
